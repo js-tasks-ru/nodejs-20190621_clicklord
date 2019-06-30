2060559363dfe13803aa15b5152da3bde52fd13d
clicklord <zakhar.it@gmail.com>
2019-06-30 10:53:18 +0300

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,17 +1,26 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const mime = require("mime-types");
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
+  const pathnameArray = url.parse(req.url).pathname.split('/');
+  if (pathnameArray.length > 2) {
+    res.statusCode = 400;
+    res.end('Bad request');
+    return;
+  }
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
-
+        sendFile(filepath, res)
+        .catch(err => {console.log(err)});
       break;
 
     default:
@@ -19,5 +28,29 @@ server.on('request', (req, res) => {
       res.end('Not implemented');
   }
 });
+
+const sendFile = async (filepath, res) => {
+  let isError = null;
+  fs.stat(filepath, (err, stats) => {
+    if (err) {
+      isError = err;
+      res.statusCode = '404';
+      res.end('File not found');
+      return;
+    }
+    if (!stats.isFile()) res.status(404).send('is not file');
+  });
+  if (isError) return;
+  const readFileStream = fs.createReadStream(filepath);
+  res.statusCode = '200';
+  const mimetype = mime.lookup(filepath); 
+  res.setHeader('Content-Type', mimetype + "; charset=utf-8");
+  readFileStream
+    .on('error', err => {
+      res.statusCode = '500';
+      res.end(err);
+    })
+    .pipe(res);
+};
 
 module.exports = server;
