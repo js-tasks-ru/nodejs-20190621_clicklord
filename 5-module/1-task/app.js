df66bf0fe4e5145def2007a1269159b9f263c15e
clicklord <zakhar.it@gmail.com>
2019-07-08 08:16:42 +0300

    some fix solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -10,14 +10,19 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/subscribe', async (ctx, next) => {
-  ctx.body = await subscribeClient();
+  try {
+    ctx.body = await subscribeClient();
+  } catch (err) {
+    ctx.body = err;
+    ctx.throw(500);
+  }
   return next();
 });
 
 router.post('/publish', async (ctx, next) => {
   if (!ctx.request.body.message) return next();
   try {
-    chatEventEmitter.emit('msg',ctx.request.body.message);
+    chatEventEmitter.emit('msg', ctx.request.body.message);
     ctx.status = 200;
     ctx.body = '';
   } catch (err) {
