b71f792ad9d5eb53bb508c26cccc71c7fc6ad8e4
clicklord <zakhar.it@gmail.com>
2019-07-14 13:50:08 +0300

    chore: some fixs

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -2,13 +2,13 @@ const User = require('../../models/User');
 
 async function checkCreditals(email, displayName, done) {
   try {
-    const findedUsers = await User.find({email: email});
-    if (!findedUsers[0] || findedUsers.length > 1) {
+    const findedUser = await User.findOne({email: email});
+    if (!findedUser) {
       const newUser = await new User({email, displayName});
       await newUser.save();
       return done(null, newUser);
     };
-    return done(null, findedUsers[0]);
+    return done(null, findedUser);
   } catch (err) {
     return done(err, false);
   }
